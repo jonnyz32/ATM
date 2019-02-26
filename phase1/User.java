93490e90b2c64ec8aba13c9c877c980257028c73
Vladimir Dyagilev <vdyagilev5@gmail.com>
2019-02-25 19:02:58 -0500

    Created User Class with method headers and variable annotations.

---
+++ phase1/User.java
@@ -0,0 +1,29 @@
+
+// TODO replace Object with Account after it is created
+public class User {
+	private ArrayList<Object> accounts;
+	private String login;
+	private String password;
+
+	public String getFullSummary(){}
+	public String getMostRecentTransaction(Object acc){}
+	public String getDateOfCreation(Object acc){}
+	public double getNetTotal(){}
+
+	public String getLogin(){}
+
+	public double viewAccountBalance(Object acc){}
+	public void transferBetweenAccounts(Object from, Object to){}
+	public void withdrawFromAccount(Object acc, double amount){}
+	public void transferToOther(Object myAcc, Object otherAcc){}
+	public void payBill(Object acc, double amount){}
+	public void depositMoney(Object acc, double money){}
+	public void requestAccountCreation(){}
+
+	// the following methods are avaliable only for BankManager
+	public void setLogin(String login)(){}
+	public void setPassword(String password){}
+	public void undoMostRecentTransaction(Object acc){}
+	//
+	
+}
