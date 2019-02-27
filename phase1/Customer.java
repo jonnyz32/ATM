e2f77029259cab4c9c0a9e78a2225f1121bbe8a4
Vladimir Dyagilev <vdyagilev5@gmail.com>
2019-02-27 16:52:47 -0500

    Wrote methods for Customer class. Need to finalize the Account classes before finishing the Customer class.

--- phase1/Customer.java
+++ phase1/Customer.java
@@ -10,19 +10,63 @@ public class Customer {
 	public String getDateOfCreation(Object acc){}
 	public double getNetTotal(){}
 
-	public String getLogin(){}
 
-	public double viewAccountBalance(Object acc){}
-	public void transferBetweenAccounts(Object from, Object to){}
-	public void withdrawFromAccount(Object acc, double amount){}
-	public void transferToOther(Object myAcc, Object otherAcc){}
-	public void payBill(Object acc, double amount){}
-	public void depositMoney(Object acc, double money){}
+	public String getLogin(){
+		return login;
+	}
+
+	public double viewAccountBalance(Object acc){
+		return acc.getBalance();
+	}
+	public boolean transferBetweenAccounts(Object from, Object to, double amount){
+		// Returns true if transfer went through, false otherwise.
+		if (from.has(amount)) {
+			to.transfer_in(amount);
+			return true;
+		}
+		return false;
+	}
+
+	// Same code for transfering between individual accounts and between two different people
+	public void transferToOther(Object myAcc, Object otherAcc, double amount){
+		return transferBetweenAccounts(myAcc, otherAcc, amount);
+	}
+
+	public boolean withdrawFromAccount(Object acc, double amount){
+		// Returns true if withdraw was succesful, false otherwise/
+		if (acc.has(amount)){
+			acc.transfer_out(amount);
+			return true
+		}
+		return false;
+	}
+
+	public void payBill(Object acc, double amount){
+		if (acc.has(amount)){
+			acc.transfer_out(amount);
+			// TODO add in the save to text file functionality
+			storeOutgoingMoney(amount);
+			return true;
+		}
+		return false;
+	}
+
+	public void depositMoney(Object acc, double money){
+		// TODO add in the text file parser to bool
+		ArrayList<double> moneyIn = getDeposits('deposits.txt');
+		for (double cheque : moneyIn){
+			acc.transfer_in(cheque);
+		}
+	}
 	public void requestAccountCreation(){}
 
 	// the following methods are avaliable only for BankManager
-	public void setLogin(String login)(){}
-	public void setPassword(String password){}
+	public void setLogin(String login)(){
+		this.login = login;
+	}
+	public void setPassword(String password){
+		this.password = password;
+	}
 	public void undoMostRecentTransaction(Object acc){}
 	//
 
