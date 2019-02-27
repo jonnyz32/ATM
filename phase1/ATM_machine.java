a7698ae0c7f750b72e06fe38a1f0ee01dd1fa2df
josiahfriesen <josiah.friesen@mail.utoronto.ca>
2019-02-26 20:38:46 -0500

    Created ATM_machine class, just with method headers

---
+++ phase1/ATM_machine.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class ATM_machine {
+
+    //protected file_manager FileManager;
+    public static void main (String[] args){
+        //file_manager.promptLogin()    (not actual method name, just filler name)
+    }
+
+    public void sendAlert(){} // stores a warning that the amount of a certain bill is too low in a text file, alerts.txt
+
+    public void getTime(){}
+
+    public void setTime(){}
+}
