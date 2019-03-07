ff01f453d61af0e8eb4d94b58898ff78766c9daf
Jonathan Zak <jonnyzak32@gmail.com>
2019-03-07 11:50:03 -0500

    catching up to master

--- phase1/Customer.java
+++ phase1/Customer.java
@@ -1,107 +1,107 @@
-////<<<<<<< HEAD
-////
-////// needs from account
-/////*
-////* has method
-////* double for amount
-////* last transaction variable
-////* revert transaction method
-////* getSummary()
-////* mostRecentTransaction()
-////* getDateOfCreation()
-////* ALSO - need getDeposits() and storeOutgoingMoney() in the text class
-////* */
-////
-////
-////// TODO replace Object with Account after it is created
-////public class Customer {
-////	private ArrayList<Object> accounts;
-////	private String login;
-////	private String password;
-////
-////	public String getFullSummary(){
-////		String summary = "";
-////		for (Account acc : accounts){
-////			summary += acc.getSummary();
-////		}
-////		return summary;
-////	}
-////
-////	public String getMostRecentTransaction(Object acc){
-////		return acc.mostRecentTransaction();
-////	}
-////
-////	public String getDateOfCreation(Object acc){
-////		return acc.getDateOfCreation();
-////	}
-////	public double getNetTotal(){
-////		// TODO
-////	}
-////
-////
-////	public String getLogin(){
-////		return login;
-////	}
-////
-////	public double viewAccountBalance(Object acc){
-////		return acc.getBalance();
-////	}
-////	public boolean transferBetweenAccounts(Object from, Object to, double amount){
-////		// Returns true if transfer went through, false otherwise.
-////		if (from.has(amount)) {
-////			to.transfer_in(amount);
-////			return true;
-////		}
-////		return false;
-////	}
-////
-////	// Same code for transfering between individual accounts and between two different people
-////	public void transferToOther(Object myAcc, Object otherAcc, double amount){
-////		return transferBetweenAccounts(myAcc, otherAcc, amount);
-////	}
-////
-////	public boolean withdrawFromAccount(Object acc, double amount){
-////		// Returns true if withdraw was succesful, false otherwise/
-////		if (acc.has(amount)){
-////			acc.transfer_out(amount);
-////			return true
-////		}
-////		return false;
-////	}
-////
-////	public void payBill(Object acc, double amount){
-////		if (acc.has(amount)){
-////			acc.transfer_out(amount);
-////			// TODO add in the save to text file functionality
-////			storeOutgoingMoney(amount);
-////			return true;
-////		}
-////		return false;
-////	}
-////
-////	public void depositMoney(Object acc, double money){
-////		// TODO add in the text file parser to bool
-////		ArrayList<double> moneyIn = getDeposits('deposits.txt');
-////		for (double cheque : moneyIn){
-////			acc.transfer_in(cheque);
-////		}
-////	}
-////
-////	public void requestAccountCreation(){
-////		// TODO use Observer strategy with bank manager
-////	}
-////
-////	// the following methods are avaliable only for BankManager
-////	public void setLogin(String login)(){
-////		this.login = login;
-////	}
-////	public void setPassword(String password){
-////		this.password = password;
-////	}
-////	public void undoMostRecentTransaction(Object acc){}
-////	//
-////
-////}
+//<<<<<<< HEAD
+//
+//// needs from account
+///*
+//* has method
+//* double for amount
+//* last transaction variable
+//* revert transaction method
+//* getSummary()
+//* mostRecentTransaction()
+//* getDateOfCreation()
+//* ALSO - need getDeposits() and storeOutgoingMoney() in the text class
+//* */
+
+//
+//// TODO replace Object with Account after it is created
+//public class Customer {
+//	private ArrayList<Object> accounts;
+//	private String login;
+//	private String password;
+//
+//	public String getFullSummary(){
+//		String summary = "";
+//		for (Account acc : accounts){
+//			summary += acc.getSummary();
+//		}
+//		return summary;
+//	}
+//
+//	public String getMostRecentTransaction(Object acc){
+//		return acc.mostRecentTransaction();
+//	}
+//
+//	public String getDateOfCreation(Object acc){
+//		return acc.getDateOfCreation();
+//	}
+//	public double getNetTotal(){
+//		// TODO
+//	}
+//
+//
+//	public String getLogin(){
+//		return login;
+//	}
+//
+//	public double viewAccountBalance(Object acc){
+//		return acc.getBalance();
+//	}
+//	public boolean transferBetweenAccounts(Object from, Object to, double amount){
+//		// Returns true if transfer went through, false otherwise.
+//		if (from.has(amount)) {
+//			to.transfer_in(amount);
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	// Same code for transfering between individual accounts and between two different people
+//	public void transferToOther(Object myAcc, Object otherAcc, double amount){
+//		return transferBetweenAccounts(myAcc, otherAcc, amount);
+//	}
+//
+//	public boolean withdrawFromAccount(Object acc, double amount){
+//		// Returns true if withdraw was succesful, false otherwise/
+//		if (acc.has(amount)){
+//			acc.transfer_out(amount);
+//			return true
+//		}
+//		return false;
+//	}
+//
+//	public void payBill(Object acc, double amount){
+//		if (acc.has(amount)){
+//			acc.transfer_out(amount);
+//			// TODO add in the save to text file functionality
+//			storeOutgoingMoney(amount);
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	public void depositMoney(Object acc, double money){
+//		// TODO add in the text file parser to bool
+//		ArrayList<double> moneyIn = getDeposits('deposits.txt');
+//		for (double cheque : moneyIn){
+//			acc.transfer_in(cheque);
+//		}
+//	}
+//
+//	public void requestAccountCreation(){
+//		// TODO use Observer strategy with bank manager
+//	}
+//
+//	// the following methods are avaliable only for BankManager
+//	public void setLogin(String login)(){
+//		this.login = login;
+//	}
+//	public void setPassword(String password){
+//		this.password = password;
+//	}
+//	public void undoMostRecentTransaction(Object acc){}
+//	//
+//
+//}
 ////=======
 //
 //// needs from account
