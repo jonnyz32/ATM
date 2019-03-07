6976eb794a179e266f953644dacf73ea5aea7227
caiphili <caiphili>
2019-03-07 18:43:02 -0500

    Removed TextInterface items from Customer

--- phase1/Customer.java
+++ phase1/Customer.java
@@ -12,12 +12,6 @@ public class Customer extends ATM_User {
 	public Customer(String username, String password){
 		super(username, password);
 		accounts = new ArrayList<GenericAccount>();
-		addAction(1, ()->getFullSummary(), "Get account summary");
-		addAction(2, ()->requestAccount(), "Request account creation");
-		for(int i=0;i<accounts.size();i++) {
-			final int f = i; //Because the input needs to be final.
-			addAction(i+2, ()->viewAccount(f), "Account: "+accounts.get(i).name);
-		}
 	}
 
 	public void requestAccount(){
