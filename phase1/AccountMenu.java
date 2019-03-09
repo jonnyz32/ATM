858fe0a7982f7cab32d238a34507b39ad60beb9b
caiphili <caiphili>
2019-03-09 12:43:18 -0500

    Refactored out user interacting elements to AccountMenu, and updated for current TextInterface

--- phase1/AccountMenu.java
+++ phase1/AccountMenu.java
@@ -1,10 +1,101 @@
+import java.util.ArrayList;
+
 public class AccountMenu extends TextInterface{
 
     private GenericAccount account;
 
     public AccountMenu(GenericAccount account){
         this.account = account;
-        // TODO: Insert actions here.
-        showMenu();
+        addAction(1, ()->showBalance(), "View Balance");
+        addAction(2, ()->getLastTransaction(), "See Last Transaction");
+        addAction(3, ()->depositCash(), "Deposit Cash");
+        addAction(4, ()->transferToSelf(), "Transfer to another account");
+        addAction(5, ()->transferToOther(), "Transfer to another user");
+        addAction(6, ()->transferToExternal(), "Pay external bill");
+        addAction(7, ()->withdraw(), "Withdraw money");
+    }
+
+    private void getLastTransaction(){
+        System.out.println("Last Transaction:");
+        String latest = account.getLatestTransaction().toString();
+        System.out.println(latest + "in" + account.name);
+    }
+
+    private void showBalance() {
+        System.out.print("This account has a ");
+        if(account.isAsset()) {
+            System.out.print("debit");
+        } else {
+            System.out.print("credit");
+        }
+        System.out.println(" balance of:");
+        System.out.println("$"+account.getBalance());
+    }
+
+    private void depositCash(){
+        System.out.println("How many Fives?");
+        int fives = nextInt();
+        System.out.println("How many Tens?");
+        int tens = nextInt();
+        System.out.println("How many Twenties?");
+        int twenties = nextInt();
+        System.out.println("How many Fifties?");
+        int fifties = nextInt();
+        account.depositCash(fives, tens, twenties, fifties);
+    }
+
+    private void transferToSelf(){
+        System.out.println("Amount to transfer?");
+        double amount = nextDouble();
+        System.out.println("Account to transfer to?");
+        String other_acc_name = nextLine();
+        account.transferToSelf(amount, other_acc_name);
+    }
+
+    private Customer transferToOther_helper() {
+        // Get the user and the account
+        System.out.println("Who would you like to transfer to");
+        String other_username = nextLine();
+        boolean other_exists = false;
+        Customer other_user = null;
+        ArrayList<ATM_User> all_users = ATM_machine.users;
+        for (ATM_User u: all_users) {
+            if (u.getUsername().equals(other_username)) {
+                other_user = (Customer) u;
+                other_exists = true;
+                break;
+            }
+        }
+        if (!other_exists) {
+            System.out.println("That username does not exist, please try another user.");
+            transferToOther_helper();
+        }
+        return other_user;
+    }
+
+    private void transferToOther(){
+        Customer other_user = transferToOther_helper();
+        String message = "Which account of " + other_user.getUsername() + " would you like to transfer to?";
+        System.out.println(message);
+        String other_acc_name = nextLine();
+        GenericAccount other_acc = other_user.getAccountByName(other_acc_name);
+        System.out.println("How much would you like to transfer?");
+        double amount = nextDouble();
+        account.transferToOther(other_user, other_acc, amount);
+    }
+
+    private void transferToExternal(){
+        System.out.println("What bill would you like to pay?");
+        String name = nextLine();
+        System.out.println("How much are you paying?");
+        double amount = nextDouble();
+        account.transferToExternal(name, amount);
+    }
+
+    private void withdraw(){
+        System.out.println("How much would you like to withdraw?");
+        System.out.println("Amount should be a multiple of 5");
+        int amount = nextInt();
+        account.withdraw(amount);
     }
 }
--- phase1/GenericAccount.java
+++ phase1/GenericAccount.java
@@ -4,12 +4,10 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class GenericAccount extends TextInterface implements Serializable {
+public class GenericAccount implements Serializable {
 
     Customer owner;
     double balance;
@@ -23,30 +21,6 @@ public class GenericAccount extends TextInterface implements Serializable {
 
     public GenericAccount() {
         super();
-        addAction(1, ()->showBalance(), "View Balance");
-        addAction(2, ()->showLastTransaction(), "See Last Transaction");
-        addAction(3, ()->depositCash(), "Deposit Cash");
-        addAction(4, ()->transferToSelf(), "Transfer to another account");
-        addAction(5, ()->transferToOther(), "Transfer to another user");
-        addAction(6, ()->transferToExternal(), "Pay external bill");
-        addAction(7, ()->withdraw(), "Withdraw money");
-    }
-
-    void showLastTransaction() {
-        System.out.println("Last Transaction:");
-        lastTransText = getLatestTransaction();
-        System.out.println(lastTransText);
-    }
-
-    void showBalance() {
-        System.out.print("This account has a ");
-        if(asset) {
-            System.out.print("debit");
-        } else {
-            System.out.print("credit");
-        }
-        System.out.println(" balance of:");
-        System.out.println("$"+balance);
     }
 
     //Updates this balance and other balance
@@ -70,16 +44,7 @@ public class GenericAccount extends TextInterface implements Serializable {
         ATM_machine.update_user(owner);
     }
 
-    void depositCash() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("How many Fives?");
-        int fives = s.nextInt();
-        System.out.println("How many Tens?");
-        int tens = s.nextInt();
-        System.out.println("How many Twenties?");
-        int twenties = s.nextInt();
-        System.out.println("How many Fifties?");
-        int fifties = s.nextInt();
+    void depositCash(int fives, int tens, int twenties, int fifties) {
         Double total = (double) (fives*5 + tens*10 + twenties*20 + fifties*50);
         if(asset) {
             balance += total;
@@ -110,12 +75,7 @@ public class GenericAccount extends TextInterface implements Serializable {
         ATM_machine.update_user(owner);
     }
 
-    void transferToSelf() {
-        Scanner i = new Scanner(System.in);
-        System.out.println("Amount to transfer?");
-        Double amount = i.nextDouble();
-        System.out.println("Account to transfer to?");
-        String other_acc_name = i.next();
+    void transferToSelf(double amount, String other_acc_name) {
         GenericAccount other_acc = owner.getAccountByName(other_acc_name);
         // Maybe print an error message if other_acc_name does not exist.
         // Add this transaction to this account
@@ -144,37 +104,7 @@ public class GenericAccount extends TextInterface implements Serializable {
         ATM_machine.update_user(owner);
     }
 
-    Customer transferToOther_helper() {
-        // Get the user and the account
-        Scanner s = new Scanner(System.in);
-        System.out.println("Who would you like to transfer to");
-        String other_username = s.next();
-        boolean other_exists = false;
-        Customer other_user = null;
-        ArrayList<ATM_User> all_users = ATM_machine.users;
-        for (ATM_User u: all_users) {
-            if (u.getUsername().equals(other_username)) {
-                other_user = (Customer) u;
-                other_exists = true;
-                break;
-            }
-        }
-        if (!other_exists) {
-            System.out.println("That username does not exist, please try another user.");
-            transferToOther_helper();
-        }
-        return other_user;
-    }
-
-    void transferToOther() {
-        Scanner s = new Scanner(System.in);
-        Customer other_user = transferToOther_helper();
-        String message = "Which account of " + other_user.getUsername() + " would you like to transfer to?";
-        System.out.println(message);
-        String other_acc_name = s.next();
-        GenericAccount other_acc = other_user.getAccountByName(other_acc_name);
-        System.out.println("How much would you like to transfer?");
-        Double amount = s.nextDouble();
+    void transferToOther(Customer other_user, GenericAccount other_acc, double amount) {
         transferBetweenHelper(other_acc, amount, other_acc.owner.getUsername());
         lastTransReverter = () -> revertTransferOther();
         ATM_machine.update_user(other_user);
@@ -186,22 +116,14 @@ public class GenericAccount extends TextInterface implements Serializable {
 
 
     }
-    void transferToExternal() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("What bill would you like to pay?");
-        String name = s.nextLine();
-        System.out.println("How much are you paying?");
-        double amount = s.nextDouble();
+
+    void transferToExternal(String name, double amount) {
         if(asset) {balance-=amount;}
         else {balance+=amount;}
         //Todo: output to outbound.txt and update user entry
     }
 
-    void withdraw() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("How much would you like to withdraw?");
-        System.out.println("Amount should be a multiple of 5");
-        int amount = s.nextInt();
+    void withdraw(int amount) {
         int[] bills = get_bill_split(amount);
         int fifties = bills[0];
         int twenties = bills[1];
