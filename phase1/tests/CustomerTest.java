20b8e779e4c4d59465a28f7ac224ad7fd0a6b00b
Vladimir Dyagilev <vdyagilev5@gmail.com>
2019-03-13 13:54:08 -0400

    Wrote some tests for Customer

--- phase1/tests/CustomerTest.java
+++ phase1/tests/CustomerTest.java
@@ -1,4 +1,37 @@
-package tests;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class CustomerTest {
+	Customer customer;
+	@Before
+	public void setup(){
+		customer = new Customer("test", "password");
+	}
+
+	@Test
+	public void testAddAccount(){
+		customer.addAccount("chequing", "c_account");
+		Assert.assertEquals(1, customer.getAccounts().size());
+		assertTrue(customer.getAccounts().get(0) instanceof ChequingAcc);
+	}
+
+	@Test
+	public void testGetFullSummary(){}
+
+	@Test
+	public void testGetNetTotal(){}
+
+	@Test
+	public void testGetAccountByName(){
+		customer.addAccount("chequing", "c_account");
+		Assert.assertEquals("c_account", customer.getAccountByName("c_account").name);
+	}
+
 }
