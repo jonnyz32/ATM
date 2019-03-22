7095d15a2ec2f42b6b7cf8a7b7f552a2b74c326b
Jonathan Zak <jonnyzak32@gmail.com>
2019-03-22 01:12:44 -0400

    Deleted redundant test case from EmployeeTest. Deleted not needed comments from Employee.

--- phase1/Employee.java
+++ phase1/Employee.java
@@ -6,31 +6,4 @@ public class Employee extends ATM_User implements Serializable, IEmployee, IAcco
     Employee(String username, String password){
         super(username, password);
     }
-
-    /*TODO: Delete these from here
-    public void requestAccount(String type, String name){
-        BankManager.requestAccount(this.getUsername(), type, name);
-    }
-
-    public boolean addAccount(String type, String name) {
-        return accountHandler.addAccount(type, name);
-    }
-
-    public ArrayList<GenericAccount> getAccounts(){
-        return accountHandler.getAccounts();
-    }
-
-    //Summary of account balances
-    public String getFullSummary(){
-        return accountHandler.getFullSummary();
-    }
-
-    //Net total of all accounts
-    public double getNetTotal() {
-        return accountHandler.getNetTotal();
-    }
-
-    public GenericAccount getAccountByName(String name) {
-        return accountHandler.getAccountByName(name);
-    }*/
 }
