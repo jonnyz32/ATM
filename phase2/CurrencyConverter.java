5535d9cc89766e11ac0f8488dfa1538f3f93486d
Jonathan Zak <jonnyzak32@gmail.com>
2019-03-23 11:20:02 -0400

    Added a method to get currency symbol to CurrencyConverter

--- phase2/CurrencyConverter.java
+++ phase2/CurrencyConverter.java
@@ -1,11 +1,22 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class CurrencyConverter {
     private StockFetcher stockFetcher;
 
+    String currencySymbolGetter(String currency){
+        Map<String, String> currencySymbol = new HashMap<>();
+        currencySymbol.put("EUR", "EURCAD");
+        currencySymbol.put("YUAN", "CNYCAD");
+        currencySymbol.put("RUBLE", "RUBCAD");
+        currencySymbol.put("BRITISH POUND", "GBPCAD");
+        return currencySymbol.get(currency);
+    }
+
     CurrencyConverter(StockFetcher stockFetcher){
         this.stockFetcher = stockFetcher;
     }
+
     double convertCurrency(String currency, int amount) throws Exception{
         return  amount * (double) stockFetcher.getCurrentStockInfo(currency).get("price");
     }
