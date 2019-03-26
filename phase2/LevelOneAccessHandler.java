7dd8b71318c2f5f6e24636f62a3018f1c1517488
caiphili <caiphili>
2019-03-25 20:41:14 -0400

    Made serializable

--- phase2/LevelOneAccessHandler.java
+++ phase2/LevelOneAccessHandler.java
@@ -1,4 +1,6 @@
-public class LevelOneAccessHandler {
+import java.io.Serializable;
+
+public class LevelOneAccessHandler implements Serializable {
 
     public void addBills(int fives, int tens, int twenties, int fifties){
         ATM_machine.depositBills(fives, tens, twenties, fifties);
