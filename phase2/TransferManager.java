660f3917af18fdacbc1a908b57c1279c573a6bf9
acostaig <ignacio.acosta@mail.utoronto.ca>
2019-03-31 15:12:25 -0400

    transfer manager refactored.

--- phase2/TransferManager.java
+++ phase2/TransferManager.java
@@ -14,24 +14,11 @@ public class TransferManager implements Serializable {
     // Generic method that takes money out of origin account and deposits to destination account.
     // Assume amount is positive
     void make_transfer() {
-        if (originAcc.type.equalsIgnoreCase("CREDIT CARD")) {
-            System.out.println("Cannot transfer from a Credit Account");
-        }
         if (originAcc.type.equalsIgnoreCase("CREDIT LINE")) {
-            originAcc.balance += amount;
             creditLinePoints(originAcc, amount);
         }
-        else {
-            originAcc.balance -= amount;
-        }
-        if (destAcc.asset) {
-            destAcc.balance += amount;
-        }
-        // Else it is a debt account
-        else {
-            destAcc.balance -= amount;
-        }
-
+        amount = Math.abs(amount);
+        transfer_helper();
         originAcc.lastTransText = "Transferred $" + Math.abs(amount) + " From:" + originAcc.name + " to " + destAcc.owner.getUsername() + ": " + destAcc.name;
         String s = originAcc.lastTransText;
         originAcc.past_trans.add(s);
@@ -41,26 +28,27 @@ public class TransferManager implements Serializable {
     }
 
      void revert_transfer() {
-        if (originAcc.type.equalsIgnoreCase("CREDIT CARD")) {
-            System.out.println("Cannot transfer from a Credit Account");
-        }
-        if (originAcc.type.equalsIgnoreCase("CREDIT LINE")) {
+        amount  = - amount;
+        transfer_helper();
+        originAcc.past_trans.remove(originAcc.past_trans.size() - 1);
+    }
+
+    private void creditLinePoints(GenericAccount acc, double amount) {
+        ((CreditLineAcc) acc).points += amount * 0.01;
+    }
+
+    void transfer_helper() {
+        if (originAcc.isAsset()) {
             originAcc.balance -= amount;
         }
         else {
             originAcc.balance += amount;
         }
         if (destAcc.isAsset()) {
-            destAcc.balance -= amount;
-        }
-        // Else it is a debt account
-        else {
             destAcc.balance += amount;
         }
-        originAcc.past_trans.remove(originAcc.past_trans.size() - 1);
-    }
-
-    private void creditLinePoints(GenericAccount acc, double amount) {
-        ((CreditLineAcc) acc).points += amount * 0.01;
+        else {
+            destAcc.balance -= amount;
+        }
     }
 }
